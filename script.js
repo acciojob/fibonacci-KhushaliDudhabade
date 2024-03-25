8376aad31dbb007776c066a2282c3c9a5b11d0b5
KhushaliDudhabade <131711092+KhushaliDudhabade@users.noreply.github.com>
2024-03-26 00:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let sum=0
 function fibonacci(num) {
 // your code here
+	if(num==0){
+		return 0
+	}
+	else if(num==1){
+		return 1
+	}
+	else{
+		sum=fibonacci(num-1)+fibonacci(num-2)
+		return sum
+	}
 }
-
+console.log(sum)
 module.exports = fibonacci;
